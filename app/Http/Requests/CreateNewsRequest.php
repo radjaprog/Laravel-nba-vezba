b2591ee9011fb828e7a5ec965b0a9779062afe51
radjaprog <vlada.radjenovic@gmail.com>
2023-07-07 12:38:21 +0200

    Create_news_user + Login requests

---
+++ app/Http/Requests/CreateNewsRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateNewsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:4',
+            'content' => 'required|min:10|max:250',
+            'team_ids' => 'required|array',
+            'team_ids.*' => 'integer|exists:teams,id'
+        ];
+    }
+}
